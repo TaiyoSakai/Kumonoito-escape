02ad41c483d78e42521a2418a1a83bc9e7a43ca5
TaiyoSakai <89883731+TaiyoSakai@users.noreply.github.com>
2022-02-24 22:40:04 +0900

    Add files via upload

---
+++ assets/js/hint.js
@@ -0,0 +1,22 @@
+'use strict'
+/* ヒントの開閉を設定します */
+{
+  const cardOpen = document.querySelectorAll(".card__title");
+  for (let i = 0; i < cardOpen.length; i++){
+    cardOpen[i].addEventListener('click', cardToggle);
+  }
+  function cardToggle(event) {
+    var content = event.currentTarget.parentNode;
+    content.classList.toggle('is-open');
+  }
+}
+{
+  const cardOpen = document.querySelectorAll(".card__title3");
+  for (let i = 0; i < cardOpen.length; i++){
+    cardOpen[i].addEventListener('click', cardToggle);
+  }
+  function cardToggle(event) {
+    var content = event.currentTarget.parentNode;
+    content.classList.toggle('is-open');
+  }
+}
